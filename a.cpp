5af00f4340022f00b712ead1ca84ece56d3c7279
Nobuhiro Oto <nobuhirooto@gmail.com>
2025-04-01 01:38:57 +0900

    added shortcuts

--- a.cpp
+++ a.cpp
@@ -2,8 +2,11 @@
 #include <atcoder/all>
 using namespace std;
 using namespace atcoder;
+#define ll long long
 #define vi vector<int>
 #define vvi vector<vector<int>>
+#define vll vector<long long>
+#define vvll vector<vector<long long>>
 #define vb vector<bool>
 #define pi pair<int,int>
 #define vpi vector<pair<int,int>>
@@ -12,7 +15,6 @@ using namespace atcoder;
 #define gpi greater<pair<int,int>>
 #define eb emplace_back
 #define mp make_pair
-#define ll long long
 #define hashset unordered_set
 #define hashmap unordered_map
 #define pq priority_queue
