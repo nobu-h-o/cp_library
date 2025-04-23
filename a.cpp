326b5e0baa1e86760d8afd05050218a1b8a14378
Nobuhiro Oto <nobuhirooto@gmail.com>
2025-04-23 22:07:06 +0900

    removed rep

--- a.cpp
+++ a.cpp
@@ -18,18 +18,6 @@ using namespace atcoder;
 #define hashset unordered_set
 #define hashmap unordered_map
 #define pq priority_queue
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define rep1(i, n) for (int i = 1; i <= (int)(n); i++)
-#define REP(i, a, b) for(int i = (int)a; i <= (int)b; i++)
-
-#define INF INT_MAX
-#define mod 998244353
-#define MOD 1000000007
-
-template<typename T>
-void chmax(T &a, const T &b) {a = (a > b? a : b);}
-template<typename T>
-void chmin(T &a, const T &b) {a = (a < b? a : b);}
 
 void solve() {
 
